29e0221a523a9cc8c72b3f3c67ae4c702a63cf75
Juani005999 <109244995+Juani005999@users.noreply.github.com>
2022-07-13 22:13:47 +0200

    Add files via upload

---
+++ TargetSelectorBusiness/AppObjFactory.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationTools;
+
+
+namespace TargetSelectorBusiness
+{
+    /// <summary>
+    /// Fabrique d'objets Business (métier) et Logic (applicatif)
+    /// </summary>
+    public class AppObjFactory : AppToolFactory
+    {
+        #region Propriétés
+        #endregion
+
+        #region Constructeur
+
+        /// <summary>
+        /// Constructeur par défaut
+        /// </summary>
+        public AppObjFactory()
+        {
+        }
+
+        #endregion
+
+        #region Champs
+        #endregion
+    }
+}
---
+++ TargetSelectorBusiness/Business/ObjTargetList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TargetSelectorBusiness
+{
+    /// <summary>
+    /// Objet représentant une liste d'objet <see cref="ObjTarget"/>
+    /// </summary>
+    class ObjTargetList
+    {
+        #region Propriétés
+        #endregion
+
+        #region Constructeur
+
+        /// <summary>
+        /// Constructeur par défaut
+        /// </summary>
+        internal ObjTargetList(AppObjFactory toolFactory)
+        {
+            this.toolFactory = toolFactory;
+        }
+
+        #endregion
+
+        #region Champs
+
+        /// <summary>
+        /// Instance de la fabrique d'objet métier
+        /// </summary>
+        private readonly AppObjFactory toolFactory = null;
+
+        #endregion
+    }
+}
